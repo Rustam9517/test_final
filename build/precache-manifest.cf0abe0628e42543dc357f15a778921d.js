ddbf9427f0cbca83ddb77f07339e52c4728eb37f
SaLiGiAisBack <36777927+SaLiGiAisBack@users.noreply.github.com>
2020-05-25 22:20:58 +0400

    in-com

---
+++ build/precache-manifest.cf0abe0628e42543dc357f15a778921d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e499e4ff07a02157afea22b319e1ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee55a530b365fc7d3fc5",
+    "url": "/static/css/main.1c46ef98.chunk.css"
+  },
+  {
+    "revision": "2a4386e9faf4587aef33",
+    "url": "/static/js/2.93291ceb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93291ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee55a530b365fc7d3fc5",
+    "url": "/static/js/main.a29e91c2.chunk.js"
+  },
+  {
+    "revision": "e7b92fe08f118a7b0777",
+    "url": "/static/js/runtime-main.375f7f6f.js"
+  }
+]);
